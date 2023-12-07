0b3a685b7ccfacf08d5f857d92f23f773296004e
abdul rohman <abdulrohmanunida@gmail.com>
2023-12-07 12:01:44 +0700

    Add binding at Detail Activity

--- app/src/main/java/com/dicoding/courseschedule/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/dicoding/courseschedule/ui/detail/DetailActivity.kt
@@ -3,14 +3,19 @@ package com.dicoding.courseschedule.ui.detail
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.dicoding.courseschedule.R
 import com.dicoding.courseschedule.data.Course
+import com.dicoding.courseschedule.databinding.ActivityDetailBinding
 import com.dicoding.courseschedule.util.DayName.Companion.getByNumber
 
 class DetailActivity : AppCompatActivity() {
 
+    private var _binding: ActivityDetailBinding? = null
+    private val binding get() = _binding!!
     companion object {
         const val COURSE_ID = "courseId"
     }
@@ -19,12 +24,16 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        _binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val courseId = intent.getIntExtra(COURSE_ID, 0)
         val factory = DetailViewModelFactory.createFactory(this, courseId)
+        viewModel = ViewModelProvider(this, factory)[DetailViewModel::class.java]
 
-
+        viewModel.course.observe(this){course->
+            showCourseDetail(course)
+        }
     }
 
     private fun showCourseDetail(course: Course?) {
@@ -33,6 +42,12 @@ class DetailActivity : AppCompatActivity() {
             val dayName = getByNumber(day)
             val timeFormat = String.format(timeString, dayName, startTime, endTime)
 
+            binding.apply {
+                tvCourseName.text = course.courseName
+                tvLecturer.text = course.lecturer
+                tvTime.text = timeFormat
+                tvNote.text = course.note
+            }
         }
     }
 
@@ -49,6 +64,7 @@ class DetailActivity : AppCompatActivity() {
                     setNegativeButton(getString(R.string.no), null)
                     setPositiveButton(getString(R.string.yes)) { _, _ ->
                         viewModel.delete()
+                        Toast.makeText(this@DetailActivity, "Course Deleted", Toast.LENGTH_SHORT).show()
                         finish()
                     }
                     show()
