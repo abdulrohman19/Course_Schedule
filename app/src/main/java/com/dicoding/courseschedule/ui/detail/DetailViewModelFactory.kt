c19a86e2491dce49ee38d16582ebcf1bc946b75b
abdul rohman <abdulrohmanunida@gmail.com>
2023-12-09 09:03:35 +0700

    Ui Test

--- app/src/main/java/com/dicoding/courseschedule/ui/detail/DetailViewModelFactory.kt
+++ app/src/main/java/com/dicoding/courseschedule/ui/detail/DetailViewModelFactory.kt
@@ -8,7 +8,9 @@ import java.lang.reflect.InvocationTargetException
 
 class DetailViewModelFactory(private val repository: DataRepository?, private val id: Int): ViewModelProvider.Factory {
 
+
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
         try {
             return modelClass.getConstructor(DataRepository::class.java, Int::class.java).newInstance(repository, id)
         } catch (e: InstantiationException) {
